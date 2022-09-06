b331ebbbbced625fb9c3299384ad0e567cfe1cc9
BT0813 <BenjaminTelford1@outlook.com>
2022-09-06 14:48:47 +0300

    home page finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 
+
 import "./navbar.css"
 
 
@@ -7,8 +8,12 @@ const Navbar= ()=>{
     return(
         <div>
             <nav>
+                <div className="logo">
+            <img  src="https://assets.ducati.com/dist/0.8.2/assets/img/ducati_id.png" alt="DUCATI" itemprop="logo"/>
+            </div>
+            <h1><Link to="/">Motorbike</Link></h1>
+          
             <Link to="/">Home</Link>
-
             <Link to="/Products">products</Link>
             <Link to="/TestDrive">TestDrive</Link>
             <Link to="/Register">Register</Link>
