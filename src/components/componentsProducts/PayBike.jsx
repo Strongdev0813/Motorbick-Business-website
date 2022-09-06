9fc4ad9f81cc005d5ee580e14214c46994063559
BT0813 <BenjaminTelford1@outlook.com>
2022-09-06 15:20:52 +0300

    resolve

---
+++ src/components/componentsProducts/PayBike.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import payBike from "./payBike.css"
+
+// const [payBike, setPayBike] = useState(false);
+// const Card = () => {
+
+//     return(
+//     <button id="buyBike" onClick={() => setPayBike(!payBike)}></button>
+//     {payBike ? (
+//         <div id="backPayCard">
+//         <div id="payCard">
+
+//         </div>
+//         </div>
+
+//         )
+//     }
+// };
